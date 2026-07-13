560ea93456235623c635c5c1ee48deda68b89875
chinkukirito <chinkukirito@gmail.com>
2026-07-13 14:25:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function checkPassword() {
 
+    let password = document.getElementById("password").value;
+
+    if (password === "kannalu") {
+
+        window.location.href = "home.html";
+
+    } else {
+
+        document.getElementById("message").innerHTML =
+        "❌ Access Denied! Incorrect Password.";
+
+    }
+
+}
